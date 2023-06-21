bdfbfb5ba510c5b3f012fdb5f194aef01c5d443d
sheteaditi <121819023+sheteaditi@users.noreply.github.com>
2023-06-21 14:49:00 +0530

    Add files via upload

---
+++ src/app/Lead/demo.js
@@ -0,0 +1,56 @@
+// Home page JS
+const newLead = document.getElementById("new-lead")
+
+newLead?.addEventListener("click", function(){
+    window.location.href = "personal_details.html";
+})
+
+//-----------------------------------------------------------------------------------------------
+
+// Personal details page JS
+const personalNxt = document.getElementById("personal-nxt")
+
+personalNxt?.addEventListener("click", function(){
+    window.location.href = "income_details.html";
+})
+
+//-----------------------------------------------------------------------------------------------
+
+// Income Details page JS
+const incomeBack = document.getElementById("income-back")
+
+incomeBack?.addEventListener("click", function(){
+    window.location.href = "personal_details.html";
+})
+
+const incomeNxt = document.getElementById("income-next")
+
+incomeNxt?.addEventListener("click", function(){
+    window.location.href = "documents.html";
+})
+
+//-----------------------------------------------------------------------------------------------
+
+// Documents page Js
+
+const documentsBack = document.getElementById("documents-back")
+
+documentsBack?.addEventListener("click", function(){
+    window.location.href = "income_details.html";
+})
+
+const documentsNxt = document.getElementById("documents-nxt")
+
+documentsNxt?.addEventListener("click", function(){
+    window.location.href = "quotation.html";
+})
+
+//-----------------------------------------------------------------------------------------------
+
+// Quotation page Js
+
+const quotationBack = document.getElementById("quotation-back")
+
+quotationBack?.addEventListener("click", function(){
+    window.location.href = "documents.html";
+})
